3f1708c4f725e7a75b554ed8984d1953c220ac2d
MatiasOso <114182943+MatiasOso@users.noreply.github.com>
2023-05-02 20:10:22 -0400

    Terminado!!!
    no se envió k wea

--- IngresoProds/formulario.js
+++ IngresoProds/formulario.js
@@ -9,29 +9,70 @@ class Product{
 }
 
 class UI {
-    addProduct(){
+    addProduct(product){
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>Product ID/strong>: ${product.id}
+                    <strong>Product Name</strong>: ${product.nombre}
+                    <strong>Product cantidad</strong>: ${product.cantidad}
+                    <strong>Product precio $</strong>: ${product.precio}
+                    <strong>Product imagen</strong>: ${product.imagen}
+                    <a href="#" class="btn btn-danger" name="delete">Eliminar</a>
+                </div>
+            </div>
+        `;
+        productList.appendChild(element);
+        this.resetForm();
+        
+    }
+    deleteProduct(element){
+        if(element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Producto eliminado', 'danger');
+        }
 
     }
-    deleteProduct(){
-
+    showMessage(message,cssClass){
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-4`;
+        div.appendChild(document.createTextNode(message));
+        // Mostrando en el DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('App');
+        container.insertBefore(div, app);
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+        },2000);
     }
-    showMessage(){
-
+    resetForm(){
+        document.getElementById('product-form').reset();
     }
 }
 
 //DOM EVENTS
-document.getElementById('product-form').addEventListener('submit',function() {
+document.getElementById('product-form').addEventListener('submit',function(e) {
    const id = document.getElementById('id').value
-   const nombre = document.getElementById('nombre').value
+   const nombre = document.getElementById('name').value
    const cantidad = document.getElementById('cantidad').value
    const precio = document.getElementById('precio').value
    const imagen = document.getElementById('imagen').value
 
-    console.log(id,nombre,cantidad,precio,imagen)
+    
+
+    const product =new Product(id,nombre,cantidad,precio,imagen);
+    const ui = new UI();
+    ui.addProduct(product);
+    ui.showMessage('Producto agregado', 'success');
+    e.preventDefault(); //para que no desaparezca esto puedo intervenir con la rapidez de la pagina ya que esto es util para almacenar en un servidor 
 });
 
-
+document.getElementById('product-list').addEventListener('click', function(e){
+    const ui = new UI();
+    ui.deleteProduct(e.target);
+});
 
 
 
